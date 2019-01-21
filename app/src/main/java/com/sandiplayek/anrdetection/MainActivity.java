923ac841acbe3ba82438018da304b0f6b439bc98
Android <sandiplayek270491@gmail.com>
2019-01-21 13:50:56 +0530

    sandip

--- app/src/main/java/com/sandiplayek/anrdetection/MainActivity.java
+++ app/src/main/java/com/sandiplayek/anrdetection/MainActivity.java
@@ -1,9 +1,7 @@
 package com.sandiplayek.anrdetection;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
