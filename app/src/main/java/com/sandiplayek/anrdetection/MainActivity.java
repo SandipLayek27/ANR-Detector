bd291fb638414876bbbe35cd55788bf363747078
Android <sandiplayek270491@gmail.com>
2019-01-21 13:25:25 +0530

    sandip

--- app/src/main/java/com/sandiplayek/anrdetection/MainActivity.java
+++ app/src/main/java/com/sandiplayek/anrdetection/MainActivity.java
@@ -14,7 +14,6 @@ import com.sandiplayek.anrwatcher.anrdetector.ANRWatchdogTestApplication;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
-    private final Object _mutex = new Object();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,47 +31,11 @@ public class MainActivity extends AppCompatActivity {
         }
         findViewById(R.id.simpleAll).setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-                /*int i = 0;
+                int i = 0;
                 while (true){
                     i++;
-                }*/
-                //SleepAMinute();
-                _deadLock();
-            }
-        });
-    }
-
-
-    private void _deadLock() {
-        new MainActivity.LockerThread().start();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override public void run() {
-                synchronized (_mutex) {
-                    Log.e("ANR-Failed", "There should be a dead lock before this message");
                 }
             }
-        }, 1000);
-    }
-    public class LockerThread extends Thread {
-        public LockerThread() {
-            setName("APP: Locker");
-        }
-        @Override
-        public void run() {
-            synchronized (_mutex) {
-                //noinspection InfiniteLoopStatement
-                while (true)
-                    SleepAMinute();
-            }
-        }
-    }
-    private static void SleepAMinute() {
-        try {
-            Thread.sleep(20 * 1000);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        });
     }
 }
